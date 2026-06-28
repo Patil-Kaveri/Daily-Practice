e37e348ee9674b40c198b65698d86bfd2dff65f0
Patil-Kaveri <patilkaveri313@gmail.com>
2026-06-28 21:33:29 +0530

    Added Day 10 practice

---
+++ Day-10/PalindromeString.java
@@ -0,0 +1,16 @@
+public class PalindromeString {
+    public static void main(String[] args) {
+        String str = "madam";
+        String reverse = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
+        }
+
+        if (str.equals(reverse)) {
+            System.out.println(str + " is a Palindrome String.");
+        } else {
+            System.out.println(str + " is not a Palindrome String.");
+        }
+    }
+}
