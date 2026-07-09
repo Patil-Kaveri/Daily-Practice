f008c58e98389d649ba71f749ddcb881542e2d4a
Patil-Kaveri <patilkaveri313@gmail.com>
2026-07-09 06:55:00 +0530

    Added Day 21 practice

---
+++ Day-21/CharacterFrequency.java
@@ -0,0 +1,20 @@
+public class CharacterFrequency {
+    public static void main(String[] args) {
+
+        String str = "programming";
+
+        int[] freq = new int[256];
+
+        for (int i = 0; i < str.length(); i++) {
+            freq[str.charAt(i)]++;
+        }
+
+        System.out.println("Character Frequencies:");
+
+        for (int i = 0; i < 256; i++) {
+            if (freq[i] != 0) {
+                System.out.println((char) i + " : " + freq[i]);
+            }
+        }
+    }
+}
