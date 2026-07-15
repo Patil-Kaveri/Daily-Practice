313f82916c4bd82d63137192bbb0fcaaf41b0f05
Patil-Kaveri <patilkaveri313@gmail.com>
2026-07-15 10:13:21 +0530

    Added Day 27 practice

--- Day-20/AutomorphicNumber.java
+++ Day-20/AutomorphicNumber.java
@@ -1,12 +1,3 @@
 public class AutomorphicNumber {
-    public static void main(String[] args) {
-        int number = 25;
-        int square = number * number;
-
-        if (String.valueOf(square).endsWith(String.valueOf(number))) {
-            System.out.println(number + " is an Automorphic Number.");
-        } else {
-            System.out.println(number + " is not an Automorphic Number.");
-        }
-    }
+    
 }
