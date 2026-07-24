c0b6e8fbbe09d592f9bd24f046b618ea5754fd62
Patil-Kaveri <patilkaveri313@gmail.com>
2026-07-24 12:32:59 +0530

    Added Day 36 practice

---
+++ Day-35/EncapsulationExample.java
@@ -0,0 +1,38 @@
+class Student {
+
+    // Private data members
+    private int id;
+    private String name;
+
+    // Setter methods
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter methods
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+public class EncapsulationExample {
+
+    public static void main(String[] args) {
+
+        Student s = new Student();
+
+        s.setId(101);
+        s.setName("Kaveri");
+
+        System.out.println("Student ID: " + s.getId());
+        System.out.println("Student Name: " + s.getName());
+    }
+}
